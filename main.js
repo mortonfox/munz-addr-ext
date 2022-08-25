d065e902be9fab76e1c790d5ba1e208e97b2bfc4
Morton Fox <github@qslw.com>
2022-08-25 19:19:20 -0400

    Add copy button

--- main.js
+++ main.js
@@ -4,9 +4,7 @@ function init() {
   'use strict';
 
   const loctext = document.getElementById('locationtext');
-  if (loctext === null) {
-    return;
-  }
+  if (loctext === null) return;
 
   const latstr = loctext.getAttribute('data-latitude');
   const lonstr = loctext.getAttribute('data-longitude');
@@ -14,15 +12,25 @@ function init() {
   const lat = parseFloat(latstr);
   const lon = parseFloat(lonstr);
 
-  function addElem(str) {
-    const elem = document.createElement('div');
-    elem.style.textAlign = 'center';
-    elem.textContent = str;
-    loctext.parentNode.insertBefore(elem, loctext);
+  function addElem(elem) {
+    const el = document.createElement('div');
+    el.style.textAlign = 'center';
+    el.appendChild(elem);
+    loctext.parentNode.insertBefore(el, loctext);
   }
 
-  const dec_coords = `${lat.toFixed(5)},${lon.toFixed(5)}`;
-  addElem(dec_coords);
+  function addText(str) {
+    addElem(document.createTextNode(str));
+  }
+
+  function flashResult(color, message) {
+    const status = document.getElementById('copy_status');
+    status.style.color = color;
+    status.textContent = message;
+    setTimeout(() => { status.textContent = ''; }, 1000);
+  }
+
+  addText(`${lat.toFixed(5)},${lon.toFixed(5)}`);
 
   function degmin(coord) {
     const deg = Math.trunc(coord);
@@ -30,8 +38,7 @@ function init() {
     return `${deg} ${min.toFixed(3)}`;
   }
 
-  const degmin_coords = `${lat >= 0 ? 'N' : 'S'} ${degmin(Math.abs(lat))} ${lon >= 0 ? 'E' : 'W'} ${degmin(Math.abs(lon))}`;
-  addElem(degmin_coords);
+  addText(`${lat >= 0 ? 'N' : 'S'} ${degmin(Math.abs(lat))} ${lon >= 0 ? 'E' : 'W'} ${degmin(Math.abs(lon))}`);
 
   chrome.storage.local.get(['bingMapsKey'], async result => {
     const key = result.bingMapsKey;
@@ -39,21 +46,40 @@ function init() {
 
     try {
       const resp = await fetch(url);
-      if (!resp.ok) {
-        throw new Error(`Status = ${resp.status} ${resp.statusText}`);
-      }
+      if (!resp.ok) throw new Error(`Status = ${resp.status} ${resp.statusText}`);
 
       const jsonobj = await resp.json();
 
       const addr = jsonobj.resourceSets[0].resources[0].name;
-      if (addr === undefined) {
-        return;
-      }
+      if (addr === undefined) return;
 
-      addElem(addr);
+      addText(addr);
+
+      let btn = document.createElement('button');
+      btn.name = btn.id = 'copy_addr';
+      btn.style.cssText = 'background-color: #E82A24; color: #fff; font-weight: 700; border: none; padding: 6px 10px; cursor: pointer';
+      btn.appendChild(document.createTextNode('Copy Addr'));
+      btn.addEventListener('click',
+        async () => {
+          try {
+            await navigator.clipboard.writeText(addr);
+            flashResult('green', 'Copied!');
+          }
+          catch (err) {
+            flashResult('red', `Copy failed! ${err.message}`);
+          }
+        }
+      );
+      let span = document.createElement('span');
+      span.appendChild(btn);
+      span.appendChild(document.createElement('br'));
+      let status = document.createElement('span');
+      status.id = status.name = 'copy_status';
+      span.appendChild(status);
+      addElem(span);
     }
     catch (err) {
-      console.log(`Fetch error: ${err.message}`);
+      addText(`Bing Maps API fetch error: ${err.message}`);
     }
   });
 }
